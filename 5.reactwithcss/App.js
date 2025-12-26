af822ab1f1c049bcfa1b9aac433fcc5172fcd947
vsolanki888 <vishal.solanki5124@gmail.com>
2025-12-26 22:03:12 +0530

    first commit

---
+++ 5.reactwithcss/App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1", {id:"heading"}, "Hello World with React and CSS!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
