3d0c32e7b941211a7ea6ab7c2680ccaa420f29aa
vsolanki888 <vishal.solanki5124@gmail.com>
2026-02-21 23:53:30 +0530

    comment added

--- src/components/__test__/Contact.test.js
+++ src/components/__test__/Contact.test.js
@@ -7,25 +7,29 @@ beforeEach(() => {
 });
 
 test("should render Contact component correctly", () => {
-  //   render(<Contact />);
+  // query by role
   const heading = screen.getByRole("heading");
+  // assertion
   expect(heading).toBeInTheDocument();
 });
 
 test("should exist button in Contact component", () => {
-  //   render(<Contact />);
+  // query by role
   const button = screen.getByRole("button");
+  // assertion
   expect(button).toBeInTheDocument();
 });
 
 test("should exist input in Contact component", () => {
-  //   render(<Contact />);
+  // query by placeholder text
   const input = screen.getByPlaceholderText("name");
+  // assertion
   expect(input).toBeInTheDocument();
 });
 
 test("should load 2 input in Contact component", () => {
-  //   render(<Contact />);
+  // query by role
   const input = screen.getAllByRole("textbox");
+  // assertion
   expect(input.length).toBe(2);
 });
